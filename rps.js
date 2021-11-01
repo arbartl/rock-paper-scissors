4e6613d1c9a1be3f9cb541eca0021a4956dfc7f9
Anthony <knoxbartl@gmail.com>
2021-11-01 10:34:25 -0500

    Added functionality to play 5 rounds and announce a winner.

--- rps.js
+++ rps.js
@@ -1,5 +1,5 @@
 // Choose a random option for the computer
-let computerChoice = computerPlay();
+let computerChoice;
 
 function computerPlay () {
     let randInt = Math.floor((Math.random() * 6) + 1);
@@ -22,38 +22,72 @@ function computerPlay () {
 
 let playerChoice;
 
-do {
-    playerChoice = prompt("Please enter your choice:");
-    playerChoice = playerChoice.toLowerCase();
+function playerPlay() {
+    do {
+        playerChoice = prompt("Please enter your choice:");
+        playerChoice = playerChoice.toLowerCase();
+    }
+    while (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors");
+    return playerChoice;
 }
-while (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors");
 
-console.log(`You chose ${playerChoice}`);
 // Compare the two choices to determine a winner
 
 function playRound(playerChoice, computerChoice) {
+    computerChoice = computerPlay();
+    playerChoice = playerPlay();
     if (playerChoice === computerChoice) {
+        playerScore++;
+        computerScore++
         return "It's a tie!";
     } else if (playerChoice === "rock") {
         if (computerChoice === "paper") {
+            computerScore++;
             return "Paper covers Rock. You Lose!";
         } else {
+            playerScore++;
             return "Rock crushes Scissors. You Win!";
         }
     } else if (playerChoice === "paper") {
         if (computerChoice === "scissors") {
+            computerScore++;
             return "Scissors cut Paper. You Lose!";
         } else {
+            playerScore++;
             return "Paper covers Rock. You Win!";
         }
     } else {
         if (computerChoice === "rock") {
+            computerScore++;
             return "Rock crushes Scissors. You Lose!";
         } else {
+            playerScore++;
             return "Scissors cut Paper. You Win!";
         }
     }
 }
 
-console.log(playRound(playerChoice, computerChoice));
-// Announce who won the game
+// Play 5 Rounds
+
+let playerScore;
+let computerScore;
+
+function game() {
+    let i;
+    playerScore = 0;
+    computerScore = 0;
+    for (i = 0; i < 5; i++) {
+        console.log(playRound(playerChoice, computerChoice));
+        console.log(`Round ${i+1} / 5 | Your score: ${playerScore} | Computer score: ${computerScore}`);
+    }
+
+    if (playerScore > computerScore) {
+        return "You win!";
+    } else if (playerScore < computerScore) {
+        return "You lose!";
+    } else {
+        return "Tie game!";
+    }
+}
+
+console.log(game());
